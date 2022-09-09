f5c3b2cc2f0d37dbbb4735d0b70e6f97f22d7047
Henry Nguyen <s3560635@student.rmit.edu.au>
2022-09-09 11:27:36 +1000

    initial commit of skeleton code task 2.1

---
+++ src/main/java/com/example/septchallenge1/SepTchallenge1Application.java
@@ -0,0 +1,13 @@
+package com.example.septchallenge1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SepTchallenge1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SepTchallenge1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/septchallenge1/SepTchallenge1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.septchallenge1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SepTchallenge1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
