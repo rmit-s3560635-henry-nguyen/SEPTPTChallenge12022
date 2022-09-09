c1fec02b16cb419cd33bdeff500305b0f565cf98
Henry Nguyen <s3560635@student.rmit.edu.au>
2022-09-09 14:05:13 +1000

    GET-item-api done

--- src/main/java/com/example/septchallenge1/controller/ItemController.java
+++ src/main/java/com/example/septchallenge1/controller/ItemController.java
@@ -1,9 +1,22 @@
 package com.example.septchallenge1.controller;
 
+import com.example.septchallenge1.dao.ItemDAO;
+import com.example.septchallenge1.model.Items;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
 public class ItemController {
 
+    @Autowired
+    private ItemDAO itemDAO;
+
+    @GetMapping(path = "/item/item", produces = "application/json")
+    public Items getItems() { return itemDAO.getAllProducts();}
+
+    
 
 }
